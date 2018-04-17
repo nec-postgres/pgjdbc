b87730a6c5747c6f1f68d38b5aaa9027f6e6bbbd
bocaph <huo-dangminh@ys.jp.nec.com>
2018-04-17 14:34:39 +0900

    Commit TDEforPG supported PostgreSQL JDBC 4.2-1 source

---
+++ README.txt
@@ -0,0 +1,24 @@
+Postgresql JDBC driver for Transparent Data Encryption for PostgreSQL
+=====================================================================
+
+This directory contains the source code of the PostgreSQL JDBC driver for 
+Transparent Data Encryption for PostgreSQL.
+
+Transparent Data Encryption for PostgreSQL(TDEforPG) is Encryption tool 
+for PostgreSQL, which adds encrypted data types to PostgreSQL. 
+For detail of information please refer to the URLs below.
+    
+    Enterprise Edition
+    http://jpn.nec.com/tdeforpg/ 
+  
+    Free Edition
+    https://github.com/nec-postgres/tdeforpg/wiki/Manual(JA)
+  
+PostgreSQL JDBC driver for TDEforPG is an standard PostgreSQL JDBC with 
+additional feature that make applications and some Object relational 
+mappers (ORM) to operate correctly with TDEforPG encrypted data.
+
+There are some version of PostgreSQL JDBC driver for TDEforPG. You can find 
+on the link below.
+ 
+       https://github.com/nec-postgres/pgjdbc 
--- ubenchmark/src/main/java/org/postgresql/benchmark/encoding/UTF8Encoding.java
+++ ubenchmark/src/main/java/org/postgresql/benchmark/encoding/UTF8Encoding.java
@@ -1,19 +1,29 @@
-/*-------------------------------------------------------------------------
-*
-* Copyright (c) 2004-2014, PostgreSQL Global Development Group
-* Copyright (c) 2004, Open Cloud Limited.
-*
-*
-*-------------------------------------------------------------------------
-*/
+/*
+ * Copyright (c) 2004, PostgreSQL Global Development Group
+ * See the LICENSE file in the project root for more information.
+ */
+// Copyright (c) 2004, Open Cloud Limited.
+
 package org.postgresql.benchmark.encoding;
 
-import org.openjdk.jmh.annotations.*;
+import org.postgresql.core.Utils;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
-import org.postgresql.core.Utils;
 
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
@@ -23,8 +33,7 @@ import java.nio.charset.CharsetEncoder;
 import java.util.concurrent.TimeUnit;
 
 /**
- * Tests the performance of UTF-8 encoding.
- * UTF-8 is used a lot, so we need to know the performance
+ * Tests the performance of UTF-8 encoding. UTF-8 is used a lot, so we need to know the performance
  */
 @Fork(value = 1, jvmArgsPrepend = "-Xmx128m")
 @Measurement(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
@@ -35,61 +44,61 @@ import java.util.concurrent.TimeUnit;
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 public class UTF8Encoding {
 
-    @Param({"1", "5", "10", "50", "100"})
-    public int length;
+  @Param({"1", "5", "10", "50", "100"})
+  public int length;
 
-    private String source;
-    private CharsetEncoder encoder;
-    private ByteBuffer buf;
-    private static final Charset UTF_8 = Charset.forName("UTF-8");
+  private String source;
+  private CharsetEncoder encoder;
+  private ByteBuffer buf;
+  private static final Charset UTF_8 = Charset.forName("UTF-8");
 
-    @Setup
-    public void setup() {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < length; i++) {
-            sb.append("Hello мир,");
-        }
-        source = sb.toString();
-        encoder = UTF_8.newEncoder();
-        buf = ByteBuffer.allocate(10240);
+  @Setup
+  public void setup() {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < length; i++) {
+      sb.append("Hello мир,");
     }
+    source = sb.toString();
+    encoder = UTF_8.newEncoder();
+    buf = ByteBuffer.allocate(10240);
+  }
 
-    @Benchmark
-    public byte[] utilsEncodeUTF8_old() {
-        ByteBuffer buf = UTF_8.encode(CharBuffer.wrap(source));
-        byte[] b = new byte[buf.limit()];
-        buf.get(b, 0, buf.limit());
-        return b;
-    }
+  @Benchmark
+  public byte[] utilsEncodeUTF8_old() {
+    ByteBuffer buf = UTF_8.encode(CharBuffer.wrap(source));
+    byte[] b = new byte[buf.limit()];
+    buf.get(b, 0, buf.limit());
+    return b;
+  }
 
-    @Benchmark
-    public byte[] utilsEncodeUTF8_current() {
-        return Utils.encodeUTF8(source);
-    }
+  @Benchmark
+  public byte[] utilsEncodeUTF8_current() {
+    return Utils.encodeUTF8(source);
+  }
 
-    @Benchmark
-    public byte[] string_getBytes() {
-        return source.getBytes(UTF_8);
-    }
+  @Benchmark
+  public byte[] string_getBytes() {
+    return source.getBytes(UTF_8);
+  }
 
-    @Benchmark
-    public ByteBuffer charset_encode() {
-        return UTF_8.encode(source);
-    }
+  @Benchmark
+  public ByteBuffer charset_encode() {
+    return UTF_8.encode(source);
+  }
 
-    @Benchmark
-    public Object encoder_byteBufferReuse() throws CharacterCodingException {
-        buf.clear();
-        return encoder.encode(CharBuffer.wrap(source), buf, true);
-    }
+  @Benchmark
+  public Object encoder_byteBufferReuse() throws CharacterCodingException {
+    buf.clear();
+    return encoder.encode(CharBuffer.wrap(source), buf, true);
+  }
 
-    public static void main(String[] args) throws RunnerException {
-        Options opt = new OptionsBuilder()
-                .include(UTF8Encoding.class.getSimpleName())
-//                .addProfiler(GCProfiler.class)
-                .detectJvmArgs()
-                .build();
+  public static void main(String[] args) throws RunnerException {
+    Options opt = new OptionsBuilder()
+        .include(UTF8Encoding.class.getSimpleName())
+        //.addProfiler(GCProfiler.class)
+        .detectJvmArgs()
+        .build();
 
-        new Runner(opt).run();
-    }
+    new Runner(opt).run();
+  }
 }
